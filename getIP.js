ead6679a4881e88b8e1d2cd151f0f3fea02890f5
Artit Kiuwilai <first087@gmail.com>
2017-01-29 14:41:02 +0700

    Refactoring

--- getIP.js
+++ getIP.js
@@ -1,14 +1,9 @@
-exports.getServerIp = function () {
-    const os = require('os');
-    const ifaces = os.networkInterfaces();
+exports.getServerIp = () => {
+    const os = require('os')
+    const ifaces = os.networkInterfaces()
 
-    let values = Object.keys(ifaces).map(function (name) {
-        return ifaces[name];
-    });
+    let values = Object.keys(ifaces).map(name => ifaces[name])
+    values = [].concat.apply([], values).filter(val => val.family == 'IPv4' && val.internal === false)
 
-    values = [].concat.apply([], values).filter(function (val) {
-        return val.family == 'IPv4' && val.internal === false;
-    });
-
-    return values.length ? values[0].address : '0.0.0.0';
+    return values.length ? values[0].address : '0.0.0.0'
 };
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const ipGetter = require('./getIP');
-const PORT = 3000;
+const express = require('express')
+const ipGetter = require('./getIP')
+const PORT = 3000
 
-const app = express();
-app.get('/', function (req, res) {
-    const ip = ipGetter.getServerIp();
-    res.send('Hello, world! My IP = ' + ip);
-});
+const app = express()
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.get('/', (req, res) => {
+    const ip = ipGetter.getServerIp()
+    res.send('Hello, world! My IP = ' + ip)
+})
+
+app.listen(PORT)
+console.log('Running on http://localhost:' + PORT)
