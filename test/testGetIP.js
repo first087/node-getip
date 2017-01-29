3a2c666c801734682d19d986bbb6e40e0b5919b2
Artit Kiuwilai <first087@gmail.com>
2017-01-29 15:00:34 +0700

    Update test script

--- test/testGetIP.js
+++ test/testGetIP.js
@@ -1,9 +1,13 @@
-const expect = require("chai").expect;
-const ipGetter = require("../getIP");
+const expect = require('chai').expect
+const ipGetter = require('../getIP')
 
 describe("Get IP", function () {
     it("should return IP address", function () {
-        const ip = ipGetter.getServerIp();
-        expect(ip).to.equal("172.20.10.4");
+        const ip = ipGetter.getServerIp()
+        const ips = ip.split('.').map(n => Number(n))
+
+        ips.forEach(ipPart => {
+            expect(ipPart).to.be.within(0, 255)
+        }, this);
     });
 });
